10f811dceb7f4dec7893f69d1fe70c7f4f59e01d
Giridharan002 <giri.2004k@gmail.com>
2024-07-23 12:05:16 +0530

    updated the liveblocks config

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -4,48 +4,26 @@ import { createRoomContext } from "@liveblocks/react";
 
 const client = createClient({
   publicApiKey: env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_API_KEY,
-  // authEndpoint: "/api/auth",
-  // throttle: 100,
 });
 
-// Presence represents the properties that exist on every user in the Room
-// and that will automatically be kept in sync. Accessible through the
-// `user.presence` property. Must be JSON-serializable.
 type Presence = {
-  // cursor: { x: number, y: number } | null,
-  // ...
+  // Add your presence properties here
 };
 
-// Optionally, Storage represents the shared document that persists in the
-// Room, even after all users leave. Fields under Storage typically are
-// LiveList, LiveMap, LiveObject instances, for which updates are
-// automatically persisted and synced to all connected clients.
 type Storage = {
-  // author: LiveObject<{ firstName: string, lastName: string }>,
-  // ...
+  // Add your storage properties here
 };
 
-// Optionally, UserMeta represents static/readonly metadata on each user, as
-// provided by your own custom auth back end (if used). Useful for data that
-// will not change during a session, like a user's name or avatar.
 type UserMeta = {
-  // id?: string,  // Accessible through `user.id`
-  // info?: Json,  // Accessible through `user.info`
+  // Add your user metadata properties here
 };
 
-// Optionally, the type of custom events broadcast and listened to in this
-// room. Use a union for multiple events. Must be JSON-serializable.
 type RoomEvent = {
-  // type: "NOTIFICATION",
-  // ...
+  // Add your room event properties here
 };
 
-// Optionally, when using Comments, ThreadMetadata represents metadata on
-// each thread. Can only contain booleans, strings, and numbers.
 export type ThreadMetadata = {
-  // resolved: boolean;
-  // quote: string;
-  // time: number;
+  // Add your thread metadata properties here
 };
 
 export const {
@@ -63,9 +41,8 @@ export const {
     useEventListener,
     useErrorListener,
     useStorage,
-    useObject,
-    useMap,
-    useList,
+    //useMap,
+    //useList,
     useBatch,
     useHistory,
     useUndo,
@@ -86,43 +63,5 @@ export const {
     useRemoveReaction,
   },
 } = createRoomContext<Presence, Storage, UserMeta, RoomEvent, ThreadMetadata>(
-  client,
-  {
-    async resolveUsers({ userIds }) {
-      // Used only for Comments. Return a list of user information retrieved
-      // from `userIds`. This info is used in comments, mentions etc.
-
-      // const usersData = await __fetchUsersFromDB__(userIds);
-      //
-      // return usersData.map((userData) => ({
-      //   name: userData.name,
-      //   avatar: userData.avatar.src,
-      // }));
-
-      return [];
-    },
-    async resolveMentionSuggestions({ text, roomId }) {
-      // Used only for Comments. Return a list of userIds that match `text`.
-      // These userIds are used to create a mention list when typing in the
-      // composer.
-      //
-      // For example when you type "@jo", `text` will be `"jo"`, and
-      // you should to return an array with John and Joanna's userIds:
-      // ["john@example.com", "joanna@example.com"]
-
-      // const userIds = await __fetchAllUserIdsFromDB__(roomId);
-      //
-      // Return all userIds if no `text`
-      // if (!text) {
-      //   return userIds;
-      // }
-      //
-      // Otherwise, filter userIds for the search `text` and return
-      // return userIds.filter((userId) =>
-      //   userId.toLowerCase().includes(text.toLowerCase())
-      // );
-
-      return [];
-    },
-  },
+  client
 );
